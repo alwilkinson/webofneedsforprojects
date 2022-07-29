279df1bb2388b0256f6eb42bb9e5549648b16524
CalebStarkey4 <78829195+CalebStarkey4@users.noreply.github.com>
2022-07-29 16:19:01 -0500

    wrote code for the functions

--- Test_Data.py
+++ Test_Data.py
@@ -1,18 +1,24 @@
 from Project_Class import Project as pr
 import pandas as pd
+from typing import List
 
+ids = {1, 2, 3}
 website = pr(1, "Website", ["JS", "C"])
 church_plant = pr(2, "Church Planting", ["Evangelism"])
 book = pr(3, "Children's Storybook", ["Creative Writing"])
 
 data = [website, church_plant, book]
-dataframe = pd.DataFrame({"Project_ID": [pr.get_id(project) for project in data], "Title": [pr.get_title(project) for project in data], "Tags":[pr.get_tags(project) for project in data]})
 
-def get_projects():
-    print("Function not created")
+def get_projects() -> List[pr]:
+    return data
 
-def create_project():
-    print("Function not created")
+def create_project(new_id: int, title: str, tags: List[str] = []):
+    if not new_id in ids:
+        data.append(pr(new_id, title, tags))
+        ids.add(new_id)
+    else:
+        print("That project ID already exists. Please try again.")
 
-def remove_project():
-    print("Function not created")
+def remove_project(project: pr):
+    data.remove(project)
+    ids.remove(pr.get_id(project))
