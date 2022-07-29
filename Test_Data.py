c16fc0d770b03951c79ab42ed7d2c49962b1599f
CalebStarkey4 <78829195+CalebStarkey4@users.noreply.github.com>
2022-07-28 21:49:21 -0500

    Create Test_Data.py
    The beginnings of a test dataset (very simplistic at the moment) with stand-ins for functions that might be useful

---
+++ Test_Data.py
@@ -0,0 +1,18 @@
+from Project_class import Project as pr
+import pandas as pd
+
+website = pr(1, "Website", ["JS", "C"])
+church_plant = pr(2, "Church Planting", ["Evangelism"])
+book = pr(3, "Children's Storybook", ["Creative Writing"])
+
+data = [website, church_plant, book]
+dataframe = pd.DataFrame({"Project_ID": [pr.get_id(project) for project in data], "Title": [pr.get_title(project) for project in data], "Tags":[pr.get_tags(project) for project in data]})
+
+def get_projects():
+    print("Function not created")
+
+def create_project():
+    print("Function not created")
+
+def remove_project():
+    print("Function not created")
