9e24621db191155b4bd8b5ca00ad9286c00593fb
Aslanfan <caleb.j.starkey119@gmail.com>
2022-08-02 22:07:29 -0500

    added a "description" field to project class

--- Project_Class.py
+++ Project_Class.py
@@ -2,12 +2,13 @@ from typing import Union, List
 
 class Project:
     """Simple object to represent a project"""
-    __slots__ = '_id', '_title', '_tags' 
+    __slots__ = '_id', '_title', '_tags', '_description'
 
-    def __init__(self, id: int, title: str, tags: List[str] = []):
+    def __init__(self, id: int, title: str, tags: List[str] = [], description: Union[str, None] = None):
         self._id = id
         self._title = title
         self._tags = tags
+        self._description = description
 
     #-------------------------- public accessors --------------------------
 
@@ -19,6 +20,9 @@ class Project:
 
     def get_title(self) -> str:
         return self._title
+
+    def get_description(self) -> str:
+        return self._description
 
     #-------------------------- nonpublic mutators --------------------------
 
--- Test_Data.py
+++ Test_Data.py
@@ -1,19 +1,19 @@
 from Project_Class import Project as pr
-from typing import List
+from typing import List, Union
 
 ids = {1, 2, 3}
-website = pr(1, "Website", ["JS", "C"])
-church_plant = pr(2, "Church Planting", ["Evangelism"])
-book = pr(3, "Children's Storybook", ["Creative Writing"])
+website = pr(1, "Website", ["JS", "C"], "Build a website.")
+church_plant = pr(2, "Church Planting", ["Evangelism"], "Planting churches in hard-to-reach areas.")
+book = pr(3, "Children's Storybook", ["Creative Writing"], "Writing Bible stories in a format children can understand.")
 
 data = [website, church_plant, book]
 
 def get_projects() -> List[pr]:
     return data
 
-def create_project(new_id: int, title: str, tags: List[str] = []):
+def create_project(new_id: int, title: str, tags: List[str] = [], description: Union[str, None] = None):
     if not new_id in ids:
-        data.append(pr(new_id, title, tags))
+        data.append(pr(new_id, title, tags, description))
         ids.add(new_id)
     else:
         print("That project ID already exists. Please try again.")
