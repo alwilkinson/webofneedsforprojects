02e16c8ae0d2c6a5d177ab3a6e1e292048f89d75
Aslanfan <caleb.j.starkey119@gmail.com>
2022-08-10 01:45:44 -0500

    updated get_project_data() and create_project()

--- project_api/Test_Data.py
+++ project_api/Test_Data.py
@@ -3,12 +3,19 @@ from typing import List, Union
 from datetime import datetime
 import project_airtable_api as airtable
 from tag_api.models import Tag
+import json
 
 def get_project_data() -> List[pr]:
-    data = airtable.get_projects()
+    data = json.loads(airtable.get_projects())["records"]
+    projects: List[pr] = []
+    for project in data:
+        fields = project["fields"]
+        projects.append(pr(project["id"], fields["Name"], fields["Tags"], fields["Description"], fields["Region"], fields["Primary_Contact"]))
+    return projects
+    
 
 def create_project(name: str, tags: List[Tag] = [], description: Union[str, None] = None, region: Union[str, None] = None, primary_contact: Union[int, None] = None, is_event: bool = False, event_time: Union[datetime, None] = None):
-    airtable.add_project()
+    airtable.add_project(name, tags = tags, description = description, region = region, primary_contact = primary_contact)
 
 def remove_project(project: pr):
-    airtable.remove_project(pr.get_id)
+    airtable.remove_project(pr.get_id(project))
